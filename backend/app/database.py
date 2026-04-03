ec3868e1e19dc8fc183a59670d179104a342e339
Arthur_Chen <nammount19@gmail.com>
2026-04-03 16:11:10 +0700

    add sll cho db

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,7 +5,11 @@ from app.core.config import settings
 password = quote_plus(settings.DATABASE_PASSWORD)
 DATABASE_URL = f"postgresql+asyncpg://{settings.DATABASE_USERNAME}:{password}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
 
-engine = create_async_engine(DATABASE_URL, echo=settings.DEBUG)
+engine = create_async_engine(
+    DATABASE_URL, 
+    echo=settings.DEBUG,
+    connect_args={"ssl": "require"}
+)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 async def get_db():
